39e864bf8b208d900a64b2e4210752a7d3252dfc
quyct <caothaiquy.java2020@gmail.com>
2022-03-26 20:54:29 +0700

    push on github

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { Component } from 'react';
+//import Portal from './components/Portal';
+// import Hello from './components/Hello';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import FRParentInput from './components/FRParentInput';
+
+class App extends Component {
+  render(){
+    return(
+      <div className='App'>
+
+      </div>
+    )
+  }
+    
+}
+export default App;
